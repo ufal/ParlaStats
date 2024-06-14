f9b0d04c5018b9e50a18f44d507dc1499c49eaae
Matej Baco <matejbaco95@gmail.com>
2024-06-14 14:40:35 +0200

    Minor modification of the client

--- api/client2.py
+++ api/client2.py
@@ -9,9 +9,11 @@ import matplotlib.pyplot as plt
 
 
 args_parser = argparse.ArgumentParser()
-args_parser.add_argument("--query_source", type=str, default="example_queries/", help="Path to query jsons which are to be processed.")
+args_parser.add_argument("--query_source", type=str, default="example_queries/CS/queries", help="Path to query jsons which are to be processed.")
 args_parser.add_argument("--URL", type=str, default="http://127.0.0.1:5000/query", help="Where is the flask server running.")
 args_parser.add_argument("--dir", type=str, default=None, help="Directory to write output to:")
+args_parser.add_argument("--specific_query", type=str, default=None,help="Help specific query to execute")
+
 class Client2():
     def __init__(self, args):
         self.URL = args.URL
@@ -22,7 +24,7 @@ class Client2():
         with open(query_file, 'r') as file:
             query = json.load(file)
         response = requests.post(self.URL, json=query)
-        return query['graph'], query['description'], response.json()
+        return query['description'], response.json()
     
     def __adjust_results(self, result):
         if result:
@@ -41,35 +43,45 @@ class Client2():
         if result:
             print(result)
         else:
-            print("Nothing to graph.")
+                print("Nothing to graph.")
+    
+    def run_specific(self, specific_query):
+        print(f"Processing {specific_query} ...")
+        description, result = self.__process_query(specific_query)
+        print(f"Result for {specific_query}:")
+        print(description)
+        res = self.__adjust_results(result)
+        print(res)
+        
 
     def run(self):
         for filename in os.listdir(self.QueryDir):
             if filename.endswith(".json"):
                 query_file = os.path.join(self.QueryDir, filename)
                 print(f"Processing {query_file} ...")
-                graph, description, result = self.__process_query(query_file)
+                description, result = self.__process_query(query_file)
                 print(f"Result for {query_file}:")
                 print(description)
-                if graph == 'N':
-                    
-                    if self.target_dir:
-                        with open(f"{self.target_dir}{filename[:-5]}_result.txt", 'w') as file:
-                            print(result, file=file)
-                            print(file=file)
-                            res = self.__adjust_results(result)
-                            print(str(res), file=file)
-                    else:
+                if self.target_dir:
+                    with open(f"{self.target_dir}{filename[:-5]}_result.txt", 'w') as file:
+                        print(result, file=file)
+                        print(file=file)
                         res = self.__adjust_results(result)
-                        print(res)
-                        print("\n")
+                        print(str(res), file=file)
                 else:
-                    self.__graph_results(result)
+                    res = self.__adjust_results(result)
+                    print(res)
+                    print("\n")
+                #else:
+                #    self.__graph_results(result)
 
 
 if __name__ == "__main__":
     args = args_parser.parse_args()
     client = Client2(args)
-    client.run()
+    if args.specific_query:
+        client.run_specific(args.specific_query)
+    else:
+        client.run()
 
     
