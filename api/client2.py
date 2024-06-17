92d9f8721f93b6a4cb929c8c07ab3c79eb0b089b
Matej Baco <matejbaco95@gmail.com>
2024-06-17 22:46:03 +0200

    Add graphing option to the client #3

--- api/client2.py
+++ api/client2.py
@@ -13,6 +13,8 @@ args_parser.add_argument("--query_source", type=str, default="example_queries/CS
 args_parser.add_argument("--URL", type=str, default="http://127.0.0.1:5000/query", help="Where is the flask server running.")
 args_parser.add_argument("--dir", type=str, default=None, help="Directory to write output to:")
 args_parser.add_argument("--specific_query", type=str, default=None,help="Help specific query to execute")
+args_parser.add_argument("--interactive", action="store_true", help="Set this flag to enable interactive mode while querying")
+
 
 class Client2():
     def __init__(self, args):
@@ -20,6 +22,8 @@ class Client2():
         self.URL = "http://127.0.0.1:5000/query"
         self.QueryDir = args.query_source
         self.target_dir = args.dir
+        self.interactive = args.interactive
+    
     def __process_query(self, query_file):
         with open(query_file, 'r') as file:
             query = json.load(file)
@@ -38,12 +42,26 @@ class Client2():
         else:
             print("Nothing to print")
 
-    def __graph_results(self, result):
+    def __graph_results(self, description, result):
         # TODO: finish
+        labels = []
+        values = []
+        label_x = list(result[0].keys())[0]
+        label_y = list(result[0].keys())[-1]
         if result:
-            print(result)
+            for sub in result:
+                #keys = list(sub.keys())
+                labels.append(sub[label_x])
+                values.append(sub[label_y])
+            
+            plt.figure(figsize=(8,6))
+            plt.bar(labels, values)
+            plt.xlabel(label_x)
+            plt.ylabel(label_y)
+            plt.title(description)
+            plt.show()
         else:
-                print("Nothing to graph.")
+            print("Nothing to graph.")
     
     def run_specific(self, specific_query):
         print(f"Processing {specific_query} ...")
@@ -60,18 +78,27 @@ class Client2():
                 query_file = os.path.join(self.QueryDir, filename)
                 print(f"Processing {query_file} ...")
                 description, result = self.__process_query(query_file)
-                print(f"Result for {query_file}:")
-                print(description)
+                
+                if self.interactive:
+                    graph = input("Would you like to graph the results?(Y/n)")
+                    if graph == 'Y':
+                        self.__graph_results(description, result)
+                    else:
+                        print(f"Result for {query_file}:")
+                        print(self.__adjust_results(result))
+                else:
+                    print(f"Result for {query_file}:")
+                    print(self.__adjust_results(result))
                 if self.target_dir:
                     with open(f"{self.target_dir}{filename[:-5]}_result.txt", 'w') as file:
                         print(result, file=file)
                         print(file=file)
                         res = self.__adjust_results(result)
                         print(str(res), file=file)
-                else:
-                    res = self.__adjust_results(result)
-                    print(res)
-                    print("\n")
+                #else:
+                #    res = self.__adjust_results(result)
+                #    print(res)
+                #    print("\n")
                 #else:
                 #    self.__graph_results(result)
 
