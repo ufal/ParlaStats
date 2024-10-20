6f4069de4f6c5e9e35c7eed8a3bbdc9c8aef91dc
Matej <matejbaco95@gmail.com>
2024-10-20 17:04:15 +0200

    Add first batch of person parser tests #9

--- test/MetadataExtraction/tester.py
+++ test/MetadataExtraction/tester.py
@@ -5,6 +5,7 @@ import argparse
 
 args_parser = argparse.ArgumentParser()
 args_parser.add_argument("--s",action="store_true", help="Set this flag to test speeches")
+args_parser.add_argument("--p",action="store_true", help="Set this flag to test persons")
 
 test_cases_speeches = [
     ("REAL","--file=examples/inputs/real.xml", "--wd=../../MetadataExtraction", "examples/expected/expectedReal.txt"),
@@ -13,14 +14,19 @@ test_cases_speeches = [
     ("MULTIPLE SPEECHES", "--file=examples/inputs/multipleSpeeches.xml",  "--wd=../../MetadataExtraction", "examples/expected/expectedMultipleSpeeches.txt"),    
 ]
 
+test_cases_persons = [
+    ("NO ISSUES", "--file=examples/inputs/noIssuesPerson.xml", "--wd=../../MetadataExtraction", "examples/expected/expectedNoIssuesPersons.txt"),
+    ("MISSING AFFILIATIONS", "--file=examples/inputs/missingAffiliations.xml", "--wd=../../MetadataExtraction", "examples/expected/expectedMissingAffiliations.txt"),
+    ("MISSING BIRTH", "--file=examples/inputs/missingBirth.xml", "--wd=../../MetadataExtraction", "examples/expected/expectedMissingBirth.txt")    
+]
 
 my_venv = "../../../bakalarka/bin/python3"
 speech_parser = "../../MetadataExtraction/speechParser2.py"
-
-def test_speeches():
-    for test_name, test_file, test_wd, expected_output in test_cases_speeches:
+person_parser = "../../MetadataExtraction/personParser2.py"
+def test(what, test_cases):
+    for test_name, test_file, test_wd, expected_output in test_cases:
         expected = ""
-        process = subprocess.run([my_venv, speech_parser, test_file, test_wd], capture_output=True, text=True)
+        process = subprocess.run([my_venv, what, test_file, test_wd], capture_output=True, text=True)
     
         actual_output = process.stdout.strip()
     
@@ -42,9 +48,12 @@ def test_speeches():
 def main(args):
     if args.s:
         print("####TESTING SPEECH PARSER####")
-        test_speeches()
+        test(speech_parser, test_cases_speeches)
         print("########## DONE #############")
-
+    if args.p:
+        print("####TESTING PERSON PARSER####")
+        test(person_parser, test_cases_persons)
+        print("########## DONE #############")
 if __name__ == "__main__":
     main(args_parser.parse_args())
 
