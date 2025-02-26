3281055fb3565f0df6124dd70e8c8742fb08ee58
Matej <matejbaco95@gmail.com>
2025-02-26 14:20:05 +0100

    Add support for agg. functions and aliases within JSON query

---
+++ api/SQLBuilder.py
@@ -0,0 +1,148 @@
+#!usr/bin/python3
+
+import argparse
+
+args_parser = argparse.ArgumentParser();
+args_parser.add_argument("--json_query", type=str, default=None, help="JSON query to buil SQL from")
+
+
+class SQLBuilder:
+    def __init__(self):
+        self.TABLE_MATCHING = {
+            "person":[],
+            "persname":["person"],
+            "organisation":[],
+            "affiliation":["person", "organisation"],
+            "speech":["person"]
+        }
+        self.TABLE_JOIN_CONDITIONS = {
+            ("person", "persname") : ("person_id", "person_id"),
+            ("person", "affiliation") : ("person_id", "person_id"),
+            ("person", "speech") : ("person_id", "person_id"),
+            ("affiliation", "organisation") : ("organisation_id", "organisation_id")
+        }
+        self.SPEECH_TIME_COLUMNS = ["time_start", "time_end", "earliest_timestamp", "latest_timestamp"]
+    
+    def determine_joins(self, columns, conditions, group_by):
+        required = set()
+        tables = self.TABLE_MATCHING.keys()
+        for table in tables:
+            for col in columns:
+                if (isinstance(col, str)):
+                    if table in col and table not in required:
+                        required.add(table)
+                elif (isinstance(col, dict)):
+                    if table in col["real"] and table not in required:
+                        required.add(table)
+            for gb in group_by:
+                if table in gb and table not in required:
+                    required.add(table)
+        
+        for cond in conditions:
+            for table in tables:
+                if (table in cond["column"]) and (table not in required):
+                    required.add(table)
+       
+        joins = []
+        if "person" in required:
+           required.remove("person")
+        
+        for table in required:
+            if table in self.TABLE_MATCHING and "person" in self.TABLE_MATCHING[table]:
+                joins.append(("person", table))
+            elif table == "organisation":
+                if ("person", "affiliation") not in joins:
+                    joins.append(("person", "affiliation"))
+                joins.append(("affiliation", table))
+            else:
+                raise ValueError(f"Unsupported table join: person -> {table}")
+        return joins
+    
+    def buildSQLQuery(self, json_query, step_results):
+        sql_query = "SELECT "
+        sql_query += self.parse_columns(json_query["columns"])
+        joins = self.determine_joins(json_query["columns"],
+                                json_query["filtering"]["conditions"],
+                                json_query["aggregation"]["group_by"])
+        
+        sql_query += self.parse_joins(joins)
+        sql_query += self.parse_conditions(json_query["filtering"]["conditions"], 
+                                           step_results)
+        
+        sql_query += self.parse_group_by(json_query["aggregation"]["group_by"])
+        sql_query += self.parse_order_by(json_query["aggregation"]["order_by"])
+        sql_query += self.parse_limit(json_query["limit"])
+
+        parameters = [cond['value'] for cond in json_query["filtering"]["conditions"] if not cond['value'].startswith("step_result.")]
+        return sql_query, parameters
+    
+    def parse_limit(self, limit):
+        res = ""
+        if limit:
+            limit_clause = "LIMIT {limit}"
+            res += limit_clause
+        return res
+
+    def parse_order_by(self, order_by):
+        res = ""
+        if order_by:
+            order_clause = " ORDER BY "
+            for ob in order_by:
+                if (isinstance(ob['column'], str)):
+                    order_clause += ob['column']
+                elif (isinstance(ob['column'], dict)):
+                    order_clause += f"{ob['column']['agg_func'](ob['column']['real'])}"
+            res += order_clause
+        return res
+
+    def parse_group_by(self, group_by):
+        res = ""
+        if group_by:
+            group_clause = " GROUP BY " + ", ".join(group_by)
+            res += group_clause
+        return res
+
+    def parse_conditions(self, conditions, step_results):
+        res = ""
+        if conditions:
+            modify = lambda x : "'"+x[0]+"'"
+            where_clause = " WHERE " + " AND ".join(
+                [
+                    f"{cond['column']} {cond['operator']} {cond['value']}" if not cond['value'].startswith("step_result.") else
+                    f"{cond['column']} {cond['operator']} ({','.join(map(modify, step_results[cond['value'].split('.')[1]]))})" if len(step_results[cond['value'].split('.')[1]]) > 1 else
+                    f"{cond['column']} {cond['operator']} '{step_results[cond['value'].split('.')[1]][0][0]}'"
+                    for cond in conditions
+                ]
+            )
+            res += where_clause
+        return res
+
+    def parse_joins(self, joins):
+        res = " FROM person "
+        for left_table, right_table in joins:
+            left_column, right_column = self.TABLE_JOIN_CONDITIONS[(left_table, right_table)]
+            res += f"LEFT JOIN {right_table} ON {left_table}.{left_column} = {right_table}.{right_column} "
+        return res
+        
+    def parse_columns(self, columns):
+        res = ""
+        for column in columns:
+            if (isinstance(column, str)):
+                res += column + ', '
+            if (isinstance(column, dict)):
+                real_part = column["real"]
+                res_temp = real_part
+                alias = column["alias"]
+                aggregation_function = column["agg_func"]
+                if (aggregation_function != ""): res_temp = f"{aggregation_function}({real_part})"
+                if (alias != ""): res_temp += f" AS {alias}"
+                res += res_temp + ', '
+        return res[:-2]
+
+
+def main(args):
+    ...   
+
+
+if __name__ == "__main__":
+    main(args_parser.parse_args())
--- api/Server2_1.py
+++ api/Server2_1.py
@@ -11,6 +11,7 @@ import json
 from datetime import datetime, time, date
 import sys
 from metainformationFetcher import metainformationFetcher
+from SQLBuilder import SQLBuilder
 
 class CustomJSONifier(JSONEncoder):
     """
@@ -47,59 +48,7 @@ class EmptyClass(object):
 args = EmptyClass()
 setattr(args,"db","../DatabaseCommunication/")
 
-TABLE_MATCHING = {
-    "person":[],
-    "persName":["person"],
-    "organisation":[],
-    "affiliation":["person", "organisation"],
-    "speech":["person"]
-}
-
-TABLE_JOIN_CONDITIONS = {
-    ("person", "persName") : ("person_id", "person_id"),
-    ("person", "affiliation") : ("person_id", "person_id"),
-    ("person", "speech") : ("person_id", "person_id"),
-    ("affiliation", "organisation") : ("organisation_id", "organisation_id")
-}
-
-SPEECH_TIME_COLUMNS = ["time_start", "time_end", "earliest_timestamp", "latest_timestamp"]
-
-def determine_joins(columns, conditions, group_by):
-    """
-    Determine the necessary joins to perform to satisfy the filtering part of
-    the json queries.
-    """
-    required = set()
-    tables = TABLE_MATCHING.keys()
-    for table in tables:
-        for col in columns:
-            if table in col and table not in required:
-                required.add(table)
-        for gb in group_by:
-            if table in gb and table not in required:
-                required.add(table)
-    
-    for cond in conditions:
-        for table in tables:
-            if (table in cond["column"]) and (table not in required):
-                required.add(table)
-
-    joins = []
-    if "person" in required:
-        required.remove("person")
-    for table in required:
-        if table in TABLE_MATCHING and "person" in TABLE_MATCHING[table]:
-            joins.append(("person", table))
-        elif table == "organisation":
-            if ("person", "affiliation") not in joins:
-                joins.append(("person", "affiliation"))
-            joins.append(("affiliation", table))
-        else:
-            raise ValueError(f"Unsuported table join: person -> {table}")
-    
-    return joins
-
-
+sql_builder = SQLBuilder()
 
 def connect_to_database(db_ini_path=args.db):
     parser = ConfigParser()
@@ -120,54 +69,6 @@ def connect_to_database(db_ini_path=args.db):
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
 
-def SQLBuilder(json_query, step_results):
-    columns = ", ".join(json_query["columns"])
-    sql_query = f"SELECT {columns} FROM person "
-    
-    # Get necessary joins
-    joins = determine_joins(json_query["columns"],
-                            json_query["filtering"]["conditions"],
-                            json_query["aggregation"]["group_by"])
-
-    # Build FROM and JOIN clauses
-    for left_table, right_table in joins:
-        left_column, right_column = TABLE_JOIN_CONDITIONS[(left_table, right_table)]
-        sql_query += f"LEFT JOIN {right_table} ON {left_table}.{left_column} = {right_table}.{right_column} "
-    
-    # Build WHERE clause
-    conditions = json_query["filtering"]["conditions"]
-    if conditions:
-        modify = lambda x : "'"+x[0]+"'"
-        where_clause = "WHERE " + " AND ".join(
-                [
-                    f"{cond['column']} {cond['operator']} {cond['value']}" if not cond['value'].startswith("step_result.") else
-                    f"{cond['column']} {cond['operator']} ({','.join(map(modify, step_results[cond['value'].split('.')[1]]))})" if len(step_results[cond['value'].split('.')[1]]) > 1 else
-                    f"{cond['column']} {cond['operator']} '{step_results[cond['value'].split('.')[1]][0][0]}'"
-                    for cond in conditions
-                ]
-        )
-        sql_query += where_clause
-    
-    # Build GROUP BY clause
-    group_by = json_query["aggregation"]["group_by"]
-    if group_by:
-        group_clause = " GROUP BY " + ", ".join(group_by)
-        sql_query += group_clause
-
-    # Build ORDER BY clause
-    order_by = json_query["aggregation"]["order_by"]
-    if order_by:
-        order_clause = " ORDER BY " + ", ".join([f"{ob['column']} {ob['direction']}" for ob in order_by])
-        sql_query += order_clause
-
-    # LIMIT
-    limit = json_query["limit"]
-    if limit:
-        limit_clause = f" LIMIT {limit}"
-        sql_query += limit_clause
-        
-    parameters = [cond['value'] for cond in conditions if not cond['value'].startswith("step_result.")]
-    return sql_query, parameters
 
 def format_output(results):
     """
@@ -201,7 +102,7 @@ def query():
         cursor = connection.cursor()
         try:
             for step in json_query['steps']:
-                sql_query, params = SQLBuilder(step, step_results)
+                sql_query, params = sql_builder.buildSQLQuery(step, step_results)
                 cursor.execute(sql_query, params)
                 step_results[step['goal']] = cursor.fetchall()
         except ValueError as e:
@@ -210,7 +111,11 @@ def query():
         connection.close()
         final_step_name = json_query['steps'][-1]['goal']
         results = step_results[final_step_name]
-        columns = [col.split(' AS ')[-1] for col in json_query['steps'][-1]['columns']]
+        columns = []
+        for column in json_query['steps'][-1]['columns']:
+            if (isinstance(column, str)): columns.append(column)
+            elif (isinstance(column, dict)): columns.append(column["alias"])
+        # columns = [col.split(' AS ')[-1] for col in json_query['steps'][-1]['columns']]
         response = [dict(zip(columns, row)) for row in results]
         res.append(database)
         res.append(format_output(response))
