49336b4736720a35ec897b9dfa7711f341ae8164
Matej <matejbaco95@gmail.com>
2025-06-10 12:57:08 +0200

    Finish (?) query making tutorial + better visuals

---
+++ WebApp/scripts/tutorials.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const formToggle = document.getElementById('form-toggle');
+	const resToggle = document.getElementById('res-toggle');
+	
+
+	formToggle.onclick = () => {
+		const formTutorial = document.getElementById('form-tutorial');
+		const resTutorial = document.getElementById('res-tutorial');
+
+		formTutorial.classList.add('active');
+		resTutorial.classList.remove('active');
+		resTutorial.style.display = "none";
+		formTutorial.style.display = "block";
+	};
+
+	resToggle.onclick = () => {
+		const formTutorial = document.getElementById('form-tutorial');
+		const resTutorial = document.getElementById('res-tutorial');
+		formTutorial.style.display = "none";
+		resTutorial.style.dispaly = "block";
+		formTutorial.classList.remove('active');
+		resTutorial.classList.add('active');
+	};
+});
