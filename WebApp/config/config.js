899fc97bd9a33e52a4714578ea5631f0eb0b7025
Matej <matejbaco95@gmail.com>
2025-06-15 09:42:00 +0200

    Fix asynchronous loading errors when using URL config files 2 #12

--- WebApp/config/config.js
+++ WebApp/config/config.js
@@ -1,5 +1,8 @@
+let _cfg;
 export async function loadConfig() {
-	const response = await fetch('config/conf.json');
-	const config = await response.json();
-	return config;
+	if (_cfg) return _cfg;
+	const res = await fetch('./config/conf.json');
+	if (!res.ok) throw new Error (`Config fetch failed ${res.status}`);
+	_cfg = await res.json();
+	return _cfg;
 }
