44defe20dacda39396d2b8ef49ac7ef88993c743
Matej <matejbaco95@gmail.com>
2025-07-16 09:39:57 +0200

    Better columns descriptions

--- WebApp/scripts/column_descriptions.js
+++ WebApp/scripts/column_descriptions.js
@@ -1,5 +1,5 @@
 let descriptions = {
-	"organization.organization_id": {
+	"organisation.organisation_id": {
 		"en":"Unique identifier for organizations"
 	},
 	"speech.id": {
@@ -9,28 +9,28 @@ let descriptions = {
 		"en":"A role that speaker held in the organization during this affiliation. \n Can have various values based on type of the organization. Minister, representative, member etc."
 	},
 	"speech.time_silent": {
-		"en":"A total amount of time when the speaker was silent during their speech in miliseconds. E.g. thinking, pause between words, etc.. Only available for ParCzech corpus!"
+		"en":"A total amount of time when the speaker was silent during their speech in miliseconds(e.g. thinking, pause between words, etc.). Only available for ParCzech corpus."
 	},
 	"speech.time_spoken": {
-		"en":"A total amount of time when the speaker was speaking during their speech in miliseconds. Only available for ParCzech corpus!"
+		"en":"A total amount of time when the speaker was speaking during their speech in miliseconds. Only available for ParCzech corpus."
 	},
 	"speech.date": {
-		"en":"Date when the speech was given in the format of YYYY-MM-DD"
+		"en":"Date when the speech was given in the format YYYY-MM-DD"
 	},
 	"person.sex": {
-		"en":"Sex of the person, M is for male, F for female, there may be some people for who this information is missing."	
+		"en":"Sex of the person, M is for male, F for female, there may be some people for who this information is missing(U as unknown)."	
 	},
 	"speech.term": {
 		"en":"Election term in which the speech was given."
 	},
 	"speech.artif_wpm": {
-		"en":"Speaking pace of the speech measured as words per minute. Only available for ParCzech corpus!"
+		"en":"Speaking pace of the speech measured as words per minute. Only available for ParCzech corpus."
 	},
 	"speech.artif_dow": {
-		"en":"Day of the week when the speech was given. A number from 0-6 where 0 is Sunday and 6 is Saturday."		
+		"en":"Day of the week when the speech was given. Monday(0) - Sunday(6)"		
 	},
 	"speech.artif_month": {
-		"en":"Month when the speech was given. A number from 1(January) to 12(December)"
+		"en":"Month when the speech was given. January(1) - December(12)"
 	},
 	"speech.artif_year": {
 		"en":"Year when the speech was given."
@@ -39,13 +39,13 @@ let descriptions = {
 		"en":"Surname of a person, for a single person, there may be multiple (i.e. marriage)"
 	},
 	"speech.time_end": {
-		"en":"End time of the speech in format HH:MM:SS",
+		"en":"End time of the speech in format HH:MM:SS. Only available for ParCzech.",
 	},
 	"person.person_id": {
 		"en":"Unique identifier of a person. Unlike forename or surename, there is guaranteed to be exactly one per person."
 	},
 	"speech.time_start": {
-		"en":"Start time of the speech in format HH:MM:SS"
+		"en":"Start time of the speech in format HH:MM:SS. Only available for ParCzech corpus."
 	},
 	"persname.forename": {
 		"en":"The forename of a person."
@@ -69,28 +69,28 @@ let descriptions = {
 		"en":"Role or the type of the organization like: parliamentary group, political party, institution, etc."
 	},
 	"persname.addname": {
-		"en":"Middle name of a person"
+		"en":"Middle name of a person."
 	},
 	"speech.role": {
-		"en":"Role of the speaker within the sitting, i.e. regular, quest etc."
+		"en":"Type of speaker who gave the speech (regular, chair, quest)."
 	},
 	"organisation.name": {
 		"en":"Name of the organization in native language."
 	},
 	"person.birth": {
-		"en":"Birth date of a person in the format of YYYY-MM-DD. For some people, this information may be missing"
+		"en":"Birth date of a person in the format of YYYY-MM-DD. For some people, this information may be missing."
 	},
 	"speech.named_entity_count": {
-		"en":"Number of named entity references within a speech"
+		"en":"Number of named entity references within a speech."
 	},
 	"speech.time_unknown": {
-		"en":"The amount of time in miliseconds, where the audio processing tool failed to determine whether speaker spoke or was silent. Only available for ParCzech!"
+		"en":"The amount of time in miliseconds, where the audio processing tool failed to determine whether speaker spoke or was silent. Only available for ParCzech."
 	},
 	"speech.earliest_timestamp": {
-		"en":"Time when the speaker started giving the speech in the format of HH:MM:SS. May not be completely accurate due to potential issues in audio alignment. Only available for ParCzech!"
+		"en":"Absolute time of the earliest <timeline> of the speech in format of HH:MM:SS. Only available for ParCzech."
 	},
 	"speech.latest_timestamp": {
-		"en":"Time when the speaker ended their speech in the format of HH:MM:SS. May not be completely accurate due to potential issues in audio alignment. Only available for ParCzech!"
+		"en":"Absolute time of the earliest <timeline> of the speech in format of HH:MM:SS. Only available for ParCzech."
 	}
 }
 
